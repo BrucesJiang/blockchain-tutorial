12338686a08514cb6910afefb0df1705920fc9b4
BrucesJiang <1948871867@qq.com>
2018-03-08 20:51:38 +0800

    Version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
 # blockchain-tutorial
 简单的区块链练习
+
+
+# Version 1.0
+1. 创建自己的区块链
+2. 理解hash函数是如何保持区块链的完整性
+3. 如何创造并添加新的块
+4. 多个节点如何竞争生成块
+5. 通过浏览器来查看整个链
+
+在当前版本中，实现了区块链的以下基本功能
+1. 块生成
+2. 散列计算
+3. 块校验
+4. 通过POST请求发送交易，通过GET请求查询所有区块的功能
+
+具体的操作信息如下图所示：
+
+启动程序：
+![启动程序](./doc/images/run-blockchain.png)
+
+发送数据生成区块
+![发送交易数据](./doc/images/POST-Add-BLock.png)
+
+查询所有交易信息
+![查询所有交易信息](/doc/images/GET-SEARCH-BLOCK.png)
+
+在实现过程中：
+
+每个区块包含的信息
+```go
+type Block struct {
+  Index     int  //这个块在链中的位置
+  Timestamp string // 块生成的时间戳
+  BPM       int   //心跳数据 BPM 心率
+  Hash      string   //这个块通过SHA256算法生成的散列值
+  PrevHash  string //前一个块的SHA256散列值
+}
+```
+
+负载信息 payload
+```go
+type Message struct {
+  BPM int
+}
+```
+
+一些依赖信息：
+
+```go
+//spew 可以帮助我们在 console 中直接查看 struct 和 slice 这两种数据结构。
+go get github.com/davecgh/go-spew/spew
+
+//Gorilla 的 mux 包非常流行， 用它来写 web handler。
+go get github.com/gorilla/mux
+
+//godotenv 读取项目根目录中的 .env 配置文件，不用将 http port 之类的配置硬编码进代码中
+go get github.com/joho/godotenv
+
+```
+需要散列的原因，主要是两个原因：
+1.在节省空间的前提下去唯一标识数据。散列是用整个块的数据计算得出，在我们的例子中，将整个块的数据通过 SHA256 计算成一个定长不可伪造的字符串。
+2.维持链的完整性。通过存储前一个块的散列值，我们就能够确保每个块在链中的正确顺序。任何对数据的篡改都将改变散列值，同时也就破坏了链。以我们从事的医疗健康领域为例，比如有一个恶意的第三方为了调整“人寿险”的价格，而修改了一个或若干个块中的代表不健康的 BPM 值，那么整个链都变得不可信了。
+
+还有一个问题没有解决：
+两个节点都生成块并添加到各自的链上，那我们应该以谁为准？始终选择最长的，但是这个问题在这里没有解决。
---
+++ main.go
@@ -0,0 +1,199 @@
+package main
+
+import (//依赖
+  "crypto/sha256"
+  "encoding/hex"
+  "encoding/json"
+  "io"
+  "log"
+  "net/http"
+  "os"
+  "strconv"
+  "sync"
+  "time"
+
+  "github.com/davecgh/go-spew/spew"
+  "github.com/gorilla/mux"
+  "github.com/joho/godotenv"
+)
+
+//数据模型, 代表每一个块的数据模型
+type Block struct {
+  Index     int  //这个块在链中的位置
+  Timestamp string // 块生成的时间戳
+  BPM       int   //心跳数据 BPM 心率
+  Hash      string   //这个块通过SHA256算法生成的散列值
+  PrevHash  string //前一个块的SHA256散列值
+}
+
+/**
+ * POST请求的handler稍微复杂， 定义POST请求的payload
+ */
+type Message struct {
+  BPM int
+}
+
+//声明一个锁变量
+var mutex = &sync.Mutex{}
+
+//Blockchain is a series of validated Blocks
+var Blockchain []Block
+
+func main() {
+  err := godotenv.Load()
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  go func() {
+    t := time.Now()
+    genesisBlock := Block{0, t.String(), 0, "", ""}
+    spew.Dump(genesisBlock)
+    Blockchain = append(Blockchain, genesisBlock)
+  }()
+  log.Fatal(run())
+}
+
+/**
+ * Gorilla/mux 初始化Web服务
+ * 其中的端口号是通过前面提到的 .env 来获得，再添加一些基本的配置参数，这个 web 服务就已经可以 listen and serve
+ */
+func run() error {
+  mux := makeMuxRouter()
+  httpAddr := os.Getenv("ADDR")
+  log.Println("Listen on", os.Getenv("ADDR"))
+  s := &http.Server{
+    Addr:           ":" + httpAddr,
+    Handler :       mux,
+    ReadTimeout:    10 * time.Second,
+    WriteTimeout:   10 * time.Second,
+    MaxHeaderBytes: 1 << 20,
+  }
+
+  if err := s.ListenAndServe(); err != nil {
+    return err
+  }
+
+  return nil
+}
+
+/**
+ *alculateHash 函数接受一个块，通过块中的 Index，Timestamp，BPM，以及 PrevHash 值来计算出 SHA256 散列值
+ */
+func calculateHash(block Block) string {
+  record := strconv.Itoa(block.Index) + block.Timestamp + strconv.Itoa(block.BPM) + block.PrevHash
+  h := sha256.New()
+  h.Write([]byte(record))
+  hashed := h.Sum(nil)
+
+  return hex.EncodeToString(hashed)
+}
+
+/**
+  * 区块生成函数
+  *Index 是从给定的前一块的 Index 递增得出，时间戳是直接通过 time.Now 函数来获得的，Hash 值通过前面的 calculateHash 函数计算得出，PrevHash 则是给定的前一个块的 Hash 值
+  */
+func generateBlock(prevBlock Block, BPM int) Block {
+  var newBlock Block
+
+  t:= time.Now()
+  newBlock.Index = prevBlock.Index + 1
+  newBlock.Timestamp = t.String()
+  newBlock.BPM = BPM
+  newBlock.PrevHash = prevBlock.Hash
+  newBlock.Hash = calculateHash(newBlock)
+
+  return newBlock
+}
+
+/**
+ * 校验块
+ * 判断一个块是否被篡改。检查Index来看这个块是否正确递增，
+ * 检查PrevHash与前一个块Hash是否一致
+ * 通过calculateHash检测当前Hash
+ */
+func isBlockValid(curBlock, prevBlock Block) bool {
+  if prevBlock.Index + 1 != curBlock.Index {
+    return false
+  }
+  if prevBlock.Hash != curBlock.PrevHash {
+    return false
+  }
+  if calculateHash(curBlock) != curBlock.Hash {
+    return false
+  }
+
+  return true
+}
+
+func replaceChain(newBlocks []Block) {
+  if len(newBlocks) > len(Blockchain) {
+    Blockchain = newBlocks
+  }
+}
+
+/**
+ * 定义不同的endpoint以及对应的handler
+ *  例如, 对于 "/" 的GET请求，我们可以查看整个链
+ *  ”/“ 的POST请求可以创建块
+ */
+func makeMuxRouter() http.Handler {
+  muxRouter := mux.NewRouter()
+  muxRouter.HandleFunc("/", handleGetBlockchain).Methods("GET")
+  muxRouter.HandleFunc("/", handleWriteBlock).Methods("POST")
+  return muxRouter
+}
+
+/**
+ * GET请求的handler
+ * 直接以 JSON 格式返回整个链，你可以在浏览器中访问 localhost:8080 或者 127.0.0.1:8080 来查看
+ */
+func handleGetBlockchain(w http.ResponseWriter, r *http.Request) {
+  bytes, err := json.MarshalIndent(Blockchain, "","  ")
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+
+  io.WriteString(w, string(bytes))
+}
+
+/*
+ * POST 请求的handler
+ */
+func handleWriteBlock(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+  var m Message
+
+  decoder := json.NewDecoder(r.Body)
+  if err := decoder.Decode(&m); err != nil {
+    responseWithJSON(w, r, http.StatusBadRequest, r.Body)
+    return
+  }
+
+  defer r.Body.Close()
+
+  mutex.Lock() //加锁
+  newBlock := generateBlock(Blockchain[len(Blockchain)-1], m.BPM)
+  mutex.Unlock() //移除锁
+
+  if isBlockValid(newBlock, Blockchain[len(Blockchain)-1]) {
+    newBlockchain := append(Blockchain, newBlock)
+    replaceChain(newBlockchain)
+    spew.Dump(Blockchain)
+  }
+
+  responseWithJSON(w, r, http.StatusCreated, newBlock)
+}
+
+
+func responseWithJSON(w http.ResponseWriter, r *http.Request, code int, payload interface{}) {
+  response, err := json.MarshalIndent(payload, "", "  ")
+  if err != nil {
+    w.WriteHeader(http.StatusInternalServerError)
+    w.Write([]byte("HTTP 500: Internal Server Error"))
+    return
+  }
+  w.WriteHeader(code)
+  w.Write(response)
+}
